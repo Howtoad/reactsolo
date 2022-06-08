16ad1eef0f6997214f936b64f850efc23878494c
Kim <howtoad123@gmail.com>
2022-06-08 10:06:42 +0200

    tests stuff

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,7 +1,12 @@
 import { Link } from "react-router-dom";
 import { css } from "@emotion/react";
+import { useContext } from "react";
+import themeContext from "./Context/themeContext";
 /** @jsxImportSource @emotion/react */
 const Navbar = () => {
+  const themechanger = (theme) => {
+    const theme = useContext(themeContext);
+  };
   const styles = {
     navbar: css`
       & a {
@@ -22,11 +27,65 @@ const Navbar = () => {
       & nav > a {
         max-width: 100px;
       }
+      & .switch {
+        position: relative;
+        display: inline-block;
+        width: 60px;
+        height: 34px;
+      }
+      & .switch input {
+        opacity: 0;
+        width: 0;
+        height: 0;
+      }
+      & .slider {
+        position: absolute;
+        cursor: pointer;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: #ccc;
+        -webkit-transition: 0.4s;
+        transition: 0.4s;
+      }
+      & .slider:before {
+        position: absolute;
+        content: "";
+        height: 26px;
+        width: 26px;
+        left: 4px;
+        bottom: 4px;
+        background-color: white;
+        -webkit-transition: 0.4s;
+        transition: 0.4s;
+      }
+      & input:checked + .slider {
+        background-color: #012039;
+      }
+      & input:focus + .slider {
+        box-shadow: 0 0 1px #2196f3;
+      }
+      & input:checked + .slider:before {
+        -webkit-transform: translateX(26px);
+        -ms-transform: translateX(26px);
+        transform: translateX(26px);
+      }
+      & .slider.round {
+        border-radius: 34px;
+      }
+      & .slider.round:before {
+        border-radius: 50%;
+      }
     `,
   };
   return (
     <nav css={styles.navbar}>
       <Link to="/home">Home</Link>
+      <label className="switch">
+        <input type="checkbox" onClick={themechanger} />
+        <span className="slider round"></span>
+      </label>
       <div>
         <Link to="home">About</Link>
         <Link to="contact">Contact</Link>
